64ec40611fd5f4b578b9a6431338acdf43ccfdac
Gustavo <luizgustavo160206@gmail.com>
2025-04-28 23:26:04 -0300

    Criacao Formulario de Denuncia

--- denguezero/urls.py
+++ denguezero/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # Adicione outras rotas aqui
+    path('reports/', include('reports.urls')),
 ]
 
 if settings.DEBUG:
