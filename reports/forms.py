9dc9161052b6febf929bf61af7ad97e65107dc32
Gustavo <luizgustavo160206@gmail.com>
2025-05-05 23:47:07 -0300

    colocando imagens no formulário

--- reports/forms.py
+++ reports/forms.py
@@ -4,4 +4,4 @@ from .models import Report
 class ReportForm(forms.ModelForm):
     class Meta:
         model = Report
-        fields = ['report_type', 'title', 'description']
+        fields = ['report_type', 'title', 'description', 'image']  # Inclua o campo 'image'
--- reports/models.py
+++ reports/models.py
@@ -10,14 +10,15 @@ class Report(models.Model):
         max_length=10,
         choices=REPORT_TYPE_CHOICES,
         verbose_name="Tipo de Denúncia",
-        default='focus'  
+        default='focus'
     )
     title = models.CharField(max_length=200, verbose_name="Título")
     description = models.TextField(verbose_name="Descrição")
     location = models.CharField(max_length=255, verbose_name="Localização", blank=True, null=True)
     latitude = models.FloatField(verbose_name="Latitude", blank=True, null=True)
     longitude = models.FloatField(verbose_name="Longitude", blank=True, null=True)
+    image = models.ImageField(upload_to='reports/', blank=True, null=True, verbose_name="Imagem")  # Campo de imagem
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Data de Criação")
 
     def __str__(self):
-        return f"{self.get_report_type_display()} - {self.title}"
+        return self.title
