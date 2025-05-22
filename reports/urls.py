e87aae6b6d1832490f6f29d18072d2e67ef8dce6
Gustavo <luizgustavo160206@gmail.com>
2025-05-21 23:40:13 -0300

    Adicionando Sistema de Login

--- reports/urls.py
+++ reports/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('', views.home, name='home'),
     path('create/', views.create_report, name='create_report'),
     path('success/', views.report_success, name='report_success'),
     path('change_status/<int:report_id>/', views.change_status, name='change_status'),
+    path('login/', views.custom_login, name='login'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
 ]
--- reports/views.py
+++ reports/views.py
@@ -4,6 +4,10 @@ from .forms import ReportForm
 from .models import Report
 from geopy.geocoders import Nominatim
 from django.db.models import Q
+from django.contrib.auth import authenticate, login
+
+
+ALLOWED_EMAILS = ['luiz@sistemasaude.com.br', 'luiz@centrozoonoses.com.br']
 
 def create_report(request):
     if request.method == 'POST':
@@ -26,10 +30,11 @@ def create_report(request):
         form = ReportForm()
     return render(request, 'reports/create_report.html', {'form': form})
 
+
 def report_success(request):
     return render(request, 'reports/report_success.html')
-  
-  
+
+
 def home(request):
     query = request.GET.get('q', '')  # Captura o termo de busca
     reports = Report.objects.all()
@@ -60,7 +65,11 @@ def home(request):
         'resolved_reports': resolved_reports,
         'case_reports': case_reports,
         'query': query,
+        
+        
+        
     })
+
 
 @login_required
 @user_passes_test(lambda u: u.is_staff)  # Apenas usuários com permissão de staff
@@ -72,3 +81,25 @@ def change_status(request, report_id):
         report.status = 'open'
     report.save()
     return redirect('home')  # Redireciona para a página principal
+
+
+def custom_login(request):
+    error = None
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        if username in ALLOWED_EMAILS:
+            user = authenticate(request, username=username, password=password)
+            if user:
+                login(request, user)
+                return redirect('home')
+            else:
+                error = "Senha incorreta."
+        else:
+            error = "Usuário não autorizado."
+    return render(request, 'reports/login.html', {'error': error})
+
+@login_required
+def sua_view_restrita(request):
+    # código da view
+    return render(request, 'reports/sua_template.html')
